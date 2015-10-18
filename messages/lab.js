82e269642cceac80e0079e839821e48ebb6fbd32
atromb01 <alexandria.trombley@tufts.edu>
2015-10-18 15:44:01 -0400

    style changes, display messages properly

--- messages/lab.js
+++ messages/lab.js
@@ -1,11 +1,14 @@
-function hello() {
-	
+function parse() {
+
+// create new XMLHttpRequest object
 var myRequest = new XMLHttpRequest();
 
+// when the XMLHttp object is ready to go, parses the JSON content
 myRequest.onreadystatechange = function() {
 	if(myRequest.readyState == 4 && myRequest.status == 200) {
 		var data = JSON.parse(myRequest.responseText);
-		document.write(data);
+		print(data);
+		
 	}
 }
 
@@ -14,22 +17,20 @@ myRequest.send();
 
 }
 
+// prints data to the screen 
+function print(data) {
 
-/*	var jsonData;
-	function reqListener() {
-		jsonData = JSON.parse(this.responseText);
-	} 
-
-	var myRequest = new XMLHttpRequest();
-	myRequest.onload = reqListener;
-	myRequest.open("get", 'data.json', true); 
-console.log(myRequest);
+	var out = " ";
 	
-	
-} */
-
-	
-	
+	// loops through the objects in the array, as created in JSON,
+	// concatonates these objects appropriately
+	for (i = 0; i < data.length; i++) {
+		out += '<span class = "mess">' + data[i].content + "  " + '</span>' + "  " + '<span class = "name">' + data[i].username + '</span>' + '<div id = "break">' + '<br>' + '<br>' + '</div>';
 		
+		// applies the concatonated strings to the HTML document
+		document.getElementById("messages").innerHTML = out;
 
-
+	}
+	
+	
+}
